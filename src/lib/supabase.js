913f0f34dae6bcff17b29b5b0cd041bf50a04d5d
MohammedElshora2005 <muhammedhosni70@gmail.com>
2026-07-16 17:22:07 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,14 +1,12 @@
-// ecommerce-store/src/lib/supabase.js
-
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("⚠️ Supabase keys are missing! Check your .env file.");
+  console.warn("⚠️ Supabase keys are missing! Check your .env file.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
 
 export default supabase;
